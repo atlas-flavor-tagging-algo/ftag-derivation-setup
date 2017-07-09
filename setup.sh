7bf09f40839b314d609f9ff962ff0cc7c13a4de9
Dan Guest <dguest@cern.ch>
2017-07-09 17:33:26 +0200

    add aliases to CVMFS setup scripts

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
+# setup ATLAS
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
 
+# make directories
 mkdir -p sources build run
 
 # check out the release and tags
 pushd .
 cd sources/
+echo -n "setting up local ATLAS environment..."
+setupATLAS -q
+lsetup asetup -q
 asetup AtlasDerivation,rel_2,21.0.X.Y-VAL,here
+echo "done"
 if [[ ! -d PhysicsAnalysis ]] ; then
     svnco DerivationFrameworkFlavourTag-99-02-17-branch
 else
